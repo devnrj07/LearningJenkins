ffacb8b35ca075a1f8b21942d57473199effddf3
nrj-od <47313186+nrj-od@users.noreply.github.com>
2019-05-27 14:36:31 +0530

    try 2

--- myWorld.java
+++ myWorld.java
@@ -5,6 +5,7 @@
   
     System.out.print("This is my world. My first jenkins Jobs");
     System.out.println("Now, my git push triggers the jenkins' build");
+    System.out.print("This should work as I reverse proxied my localhost");
 } 
 
 }
