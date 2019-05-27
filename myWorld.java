d1edd9bc6b2f94acb2deea48a9327405ccdc97d5
nrj-od <47313186+nrj-od@users.noreply.github.com>
2019-05-27 12:48:08 +0530

    should trigger jenkins' build

--- myWorld.java
+++ myWorld.java
@@ -4,6 +4,7 @@
  public static void main(String[] args) {
   
     System.out.print("This is my world. My first jenkins Jobs");
+    System.out.println("Now my git push triggers the jenkins' build");
 } 
 
 }
